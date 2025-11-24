4681f39593d6babd8f84ac5da03f31b157bc4dda
Masanori Ikeda <sintokui3141592@gmail.com>
2025-11-24 16:52:20 +0900

    second commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div>
+      <h1>Counter</h1>
+      <button v-on:click="increment">
+        {{count}}
+      </button>
+    </div>
+
+  </div>
+</template>
+
+<!--  //Composition APIの書き方
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment(){
+  count.value++
+}
+</script>     -->
+
+<script>
+export default {
+  data: () => ({
+    count: 0,
+  }),
+  methods: {
+    increment: function(){
+      this.count++
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
