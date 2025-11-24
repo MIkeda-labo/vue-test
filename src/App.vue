b4c114397fa0bfff9ec606961bb3d7b285b4f930
Masanori Ikeda <sintokui3141592@gmail.com>
2025-11-24 20:05:11 +0900

    fifth commit

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,16 @@
         {{count}}
       </button>
     </div>
-
+    <div>
+      <h1>{{title}}</h1>
+      <input type="text" v-model="inputText">
+      <button v-on:click="onClick">Add</button>
+      <TodoItem
+        v-for="(task, key) in tasks"
+        :task="task"
+        :key="key"
+        @delete-task="deleteTask"></TodoItem>
+    </div>
   </div>
 </template>
 
@@ -22,13 +31,31 @@ function increment(){
 </script>     -->
 
 <script>
+import TodoItem from './components/ToDoItem.vue'
 export default {
+  components: {
+        TodoItem
+  },
   data: () => ({
     count: 0,
+    title: "To Do Appli",
+    inputText: "",
+    tasks: [],
+    is_checked: false,
   }),
   methods: {
     increment: function(){
       this.count++
+    },
+    onClick: function(){
+      this.message = this.inputText
+      this.tasks.push(this.inputText)
+    },
+    deleteTask: function(taskToDelete){
+      const index = this.tasks.indexOf(taskToDelete)
+      if(index !== -1){
+        this.tasks.splice(index, 1)
+      }
     }
   }
 }
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <input type="checkbox">
+        <span>{{task}}</span>
+        <button v-on:click="handleDelete">Delete</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['task'],
+    methods: {
+        handleDelete: function(){
+            this.$emit('delete-task', this.task)
+        }
+    }
+}
+</script>
